5eb285afb26b6fb0c1db1a73795487909244b279
Collin OConnell <ococncol@gmail.com>
2020-05-27 16:07:02 -0400

    trying to load data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import gql from 'graphql-tag'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/players/:playerId">
+            <PlayerDetail/>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const ALL_PLAYERS = gql`
+  query {
+    players {
+      id
+      name
+    }
+  }
+`
+
+function Home() {
+  const { loading, error, data } = useQuery(ALL_PLAYERS)
+  return (
+    <>
+      <h2>Players</h2>
+      {loading && (
+        <p>Loading...</p>
+      )}
+      {error && (
+        <p>Error... Something happened..</p>
+      )}
+      {data && (
+        <>
+          {data.players.map(({ id,name }) => (
+            <p key={id}>
+              <Link to={`/players/${id}`}>
+                {name}
+              </Link>
+            </p>
+          ))}
+        </>
+      )}
+    </>
+  )
+}
+
+const GET_PLAYER = gql`
+  {
+    player(id: id) {
+      id
+      name
+    }
+  }
+`
+
+function PlayerDetail() {
+  const { playerId } = useParams();
+  const { loading, error, data } = useQuery(GET_PLAYER, {
+    variables: {
+      id: playerId
+    }
+  });
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error...</p>
+
+  return (
+    <>
+      <h2>{data.player.name}</h2>
+    </>
+  )
+
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import ApolloClient, { gql } from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:7777/graphql/"
+  }),
+  cache: new InMemoryCache()
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
